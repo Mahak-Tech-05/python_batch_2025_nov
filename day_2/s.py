a05415c4e319d6ed285fe0189e2423db7ff0e5a7
Mahak Tech <saxenamahak612@gmail.com>
2025-11-12 13:35:59 +0530

    Add files via upload

---
+++ day_2/s.py
@@ -0,0 +1,9 @@
+#i take variable name :-> user_name
+user_name = "XYZ" # in this variable i alerady give a data (value) of string type
+# Now this is second variable i take
+age = 18 # this is integer type of data(value)
+# Now this is third variable
+d = 0.5#floaat
+print(user_name)
+print(age)
+print(d)
